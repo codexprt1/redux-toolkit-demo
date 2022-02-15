efcbc1a14ae29b9632b67b8412f0bc803bdc4900
codexprt1 <codexprt1@gmail.com>
2022-02-15 14:45:15 +0530

    added redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counter";
 
 const App = () => {
+	const { value } = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h1>Redux Toolkit Demo</h1>
+			<h3>The Value is {value}</h3>
+			<button onClick={() => dispatch(increment())}>Increment</button>
+			<br />
+			<br />
+			<button onClick={() => dispatch(decrement())}>Decrement</button>
 		</div>
 	);
 };
